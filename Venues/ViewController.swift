d77f181f23a8bbc5f3f0d19b538e3a95a5e5942c
Aivars <aivars.meijers@gmail.com>
2018-11-23 20:20:33 +0200

    Initial Commit

---
+++ Venues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Venues
+//
+//  Created by Aivars Meijers on 23/11/2018.
+//  Copyright © 2018 Aivars Meijers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
